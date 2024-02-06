48fab1546da9a20928b2579c7f0b940c7b295cd3
JulieH667 <157452305+JulieH667@users.noreply.github.com>
2024-02-05 23:59:12 -0800

    Add files via upload
    This code was difficult for me as a beginner, but it makes sense after hours of staring at it. I had to look elsewhere for help such as the web and YouTube. I used the function getAnIntFromTheUser and compareTwoInts to compare both integers and lastly sumTwoInts to sum both of the integers. I tested it out with different numbers and it ran the code the way I wanted it to.

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
-public class Main {
+public class Main { public static int compareTwoInts(int a, int b) {
+        return Math.max(a, b);
+    } public static int sumTwoInts(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        String originalStr = "Hello";
-        String reversedStr = "";
-        System.out.println("Original string: " + originalStr);
 
-        for (int i = 0; i < originalStr.length(); i++) {
-            reversedStr = originalStr.charAt(i) + reversedStr;
-        }
+        int num1 = 6798;
+        int num2 = 24789;
 
-        System.out.println("Reversed string: " + reversedStr);
+        System.out.println("integer: " + num1);
+        System.out.println("integer: " + num2);
+
+        int largerInt = compareTwoInts(num1, num2);
+        System.out.println("Larger integer: " + largerInt);
+
+        int sum = sumTwoInts(num1, num2);
+        System.out.println("Sum of the two integers: " + sum);
     }
-    }
+}
